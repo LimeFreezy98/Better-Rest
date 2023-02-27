241e452c10ac892b395f883fd1195ccb431d724e
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-02-26 20:14:43 -0700

    Initial Commit

---
+++ Better Rest 2/Better_Rest_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Rest_2App.swift
+//  Better Rest 2
+//
+//  Created by Vasiliy on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Better_Rest_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
